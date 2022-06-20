f7c59b0f1023e044f85289e7a2deb666be16e943
daniarfadd <daniarfadaffa@yahoo.com>
2022-06-20 20:42:23 +0700

    filled all the boxes with color

---
+++ src/Box.js
@@ -0,0 +1,14 @@
+function Box(props) {
+
+    const style = {
+        backgroundColor: "green"
+    }
+
+    return ( 
+        <div className="box" style={style}>
+            
+        </div>
+     );
+}
+
+export default Box;
